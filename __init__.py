592e92961732cbc5ffabab59e2f86483e6d63171
James Watson <james.r.watson@utah.edu>
2016-06-25 15:54:46 -0600

    Template "__init__.py" file for making new modules

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Presence of file in directory notfies Python that this directory contains a module
+
+# List all the submodules for a 'from MODULENAME import *' command
+# __all__ = ["submodule1", "submodule2", "submodule3"]
