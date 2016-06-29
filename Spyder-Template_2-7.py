fbe984c24928a2791ef1daaadda21a6000d36d3f
James Watson <james.r.watson@utah.edu>
2016-06-28 22:29:22 -0600

    Added additional search paths for ResearchEnv

--- Spyder-Template_2-7.py
+++ Spyder-Template_2-7.py
@@ -29,7 +29,8 @@ def add_first_valid_dir_to_path(dirList):
         print "None of the specified directories were loaded"
 # List all the places where the research environment could be
 add_first_valid_dir_to_path( [ '/home/jwatson/regrasp_planning/researchenv',
-                               '/media/jwatson/FILEPILE/Python/ResearchEnv' ] )
+                               '/media/jwatson/FILEPILE/Python/ResearchEnv',
+                               '/media/mawglin/FILEPILE/Python/ResearchEnv'] )
 from ResearchEnv import * # Load the custom environment
 
 localize(__file__) # You can now load local modules!
