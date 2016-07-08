b38acf432dec914a1bf4d43050ccc585811156fe
James Watson <james.r.watson@utah.edu>
2016-07-08 17:51:58 -0600

    Template for modules

---
+++ Module-Template_2-7.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Template Version: 2016-07-08
+
+# ~~ Future First ~~
+from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
+
+"""
+FILENAME.py
+James Watson, YYYY MONTHNAME
+A ONE LINE DESCRIPTION OF THE FILE
+"""
