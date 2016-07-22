6c596e28e876ca8f55c3f0735b6bc52c14c28f13
James Watson <james.r.watson@utah.edu>
2016-07-22 14:13:19 -0600

    Separated the path-appending function so that a dir-finding function could be used separately

--- Spyder-Template_2-7.py
+++ Spyder-Template_2-7.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Template Version: 2016-06-25
+# Template Version: 2016-07-22
 
 # ~~ Future First ~~
 from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
@@ -15,22 +15,34 @@ A ONE LINE DESCRIPTION OF THE FILE
 import sys, os.path
 SOURCEDIR = os.path.dirname(os.path.abspath(__file__)) # URL, dir containing source file: http://stackoverflow.com/a/7783326
 
+def first_valid_dir(dirList):
+    """ Return the first valid directory in 'dirList', otherwise return False if no valid directories exist in the list """
+    rtnDir = False
+    for drctry in dirList:
+        if os.path.exists( drctry ):
+            rtnDir =  drctry
+            break
+    return rtnDir
+        
 def add_first_valid_dir_to_path(dirList):
     """ Add the first valid directory in 'dirList' to the system path """
     # In lieu of actually installing the library, just keep a list of all the places it could be in each environment
-    loadedOne = False
-    for drctry in dirList:
-        if os.path.exists( drctry ):
-            sys.path.append( drctry )
-            print 'Loaded', str(drctry)
-            loadedOne = True
-            break
-    if not loadedOne:
-        print "None of the specified directories were loaded"
+    validDir = first_valid_dir(dirList)
+    if validDir:
+        sys.path.append( validDir )
+        print 'Loaded', str(validDir)
+    else:
+        raise ImportError("None of the specified directories were loaded") # Assume that not having this loaded is a bad thing
 # List all the places where the research environment could be
 add_first_valid_dir_to_path( [ '/home/jwatson/regrasp_planning/researchenv',
                                '/media/jwatson/FILEPILE/Python/ResearchEnv',
-                               'F:\Python\ResearchEnv' ] )
-from ResearchEnv import * # Load the custom environment
+                               'F:\Python\ResearchEnv',
+                               '/media/mawglin/FILEPILE/Python/ResearchEnv'] )
+
+# ~~ Libraries ~~
+# ~ Standard Libraries ~
+# ~ Special Libraries ~
+# ~ Local Libraries ~
+from ResearchEnv import * # Load the custom environment, this also loads UCBerkeleyUtil
 
 # == End Init ==========================================================================================================
