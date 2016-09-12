5a271c9c195b5912ac4df436fa8e5438b08b0747
flatmachine_windows\JWatson <james.r.watson@utah.edu>
2016-09-12 14:28:14 -0600

    wing template

---
+++ Wing-Template_2-7.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Template Version: 2016-09-07
+
+# ~~ Future First ~~
+from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
+
+"""
+FILENAME.py , Built on Wing 101 for Python 2.7
+James Watson, YYYY MONTHNAME
+A ONE LINE DESCRIPTION OF THE FILE
+"""
+
+# == Init Environment ==================================================================================================
+import sys, os.path
+SOURCEDIR = os.path.dirname(os.path.abspath(__file__)) # URL, dir containing source file: http://stackoverflow.com/a/7783326
+# == End Init ==========================================================================================================
