c76949fd7f9df7c13682af1c5a5780ae95309919
James Watson <james.watson-2@colorado.edu>
2018-11-25 19:56:52 -0700

    Search ICRA for titles

--- read_PDF_metadata.py
+++ read_PDF_metadata.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__progname__ = "PROGRAM_NAME.py"
-__version__  = "YYYY.MM"
-__desc__     = "A_ONE_LINE_DESCRIPTION_OF_THE_FILE"
+__progname__ = "read_PDF_metadata.py"
+__version__  = "2018.11"
+__desc__     = "Read PDF titles in a directory"
 """
 James Watson , Template Version: 2018-05-14
 Built on Wing 101 IDE for Python 3.6
@@ -29,9 +29,11 @@ def prepend_dir_to_path( pathName ): sys.path.insert( 0 , pathName ) # Might nee
 # ~~~ Imports ~~~
 # ~~ Standard ~~
 from math import pi , sqrt
+import os
 # ~~ Special ~~
 import numpy as np
 # ~~ Local ~~
+from pdfrw import PdfReader
 
 # ~~ Constants , Shortcuts , Aliases ~~
 EPSILON = 1e-7
@@ -46,21 +48,49 @@ def __prog_signature__(): return __progname__ + " , Version " + __version__ # Re
 
 # === Main Application =====================================================================================================================
 
-# = Program Functions =
-
-
-
-# _ End Func _
-
 # = Program Vars =
 
-
+SEARCHDIR = "/media/jwatson/FILEPILE/ICRA_2018/"
+ALLFILES  = [ os.path.join( SEARCHDIR , fName ) for fName in os.listdir( SEARCHDIR ) ]
+print( "Located" , len( ALLFILES ) , "files in the search dir!" )
 
 # _ End Vars _
 
+# = Program Functions =
+
+def search_titles_for_terms( *terms ):
+    """ Search all the titles for the 'terms' """
+    # NOTE: Linear search
+    count = 0
+    for fName in ALLFILES:
+        f = PdfReader( fName )
+        for term in terms:
+            if term in f.Info['/Title']:
+                print( f.Info['/Title'] )
+                print( f.Info['/Author'] )
+                print( f.Info['/Keywords'] )
+                print()
+                count += 1
+    print( count , "files match the terms" )
+            
+
+# _ End Func _
+
 if __name__ == "__main__":
-    print __prog_signature__()
+    print( __prog_signature__() )
     termArgs = sys.argv[1:] # Terminal arguments , if they exist
+    
+    # Open the PDF
+    x = PdfReader( "/media/jwatson/FILEPILE/ICRA_2018/0005.pdf" )
+    print( x.keys() )
+    print( x.Info )
+    print( x.Info['/Title'] )
+    print( x.Info['/Author'] )
+    print( x.Info['/Keywords'] )
+    print()
+    
+    search_titles_for_terms( "assembly" , "Assembly" )
+    
     
 
 # ___ End Main _____________________________________________________________________________________________________________________________
