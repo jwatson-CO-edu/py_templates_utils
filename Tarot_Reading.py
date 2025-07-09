6420fac8161a554a3d3ec4ed883b275802f204ed
James Watson <james.watson-2@colorado.edu>
2025-07-09 12:05:00 -0600

    `turtle` program to depict the current mood

---
+++ Tarot_Reading.py
@@ -0,0 +1,7 @@
+from turtle import screensize, pendown, penup, pensize, forward, right, left, speed, bgcolor
+_SIZE_MIN =   2
+_SIZE_MAX =   6
+_LINE_MIN =  50
+_LINE_MAX = 250
+_TURN_MIN =   5
+_TURN_MAX = 175
