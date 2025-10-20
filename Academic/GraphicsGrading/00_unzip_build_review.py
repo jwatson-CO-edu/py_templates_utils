eb69634469fb63a14c1224c92edb9b2f74d5ad6a
James Watson <james.watson-2@colorado.edu>
2025-10-20 16:22:40 -0600

    Small reorg, needs testing

--- Academic/GraphicsGrading/00_unzip_build_review.py
+++ Academic/GraphicsGrading/00_unzip_build_review.py
@@ -11,257 +11,18 @@ from pprint import pprint
 ### Special ###
 import numpy as np
 ### Local ###
-from utils import read_config_into_env, env_get, out_line
+from utils import ( read_config_into_env, env_get, out_line, levenshtein_search_dist, concat_structs,
+                    tokenize, filter_float_tokens, vec_unit, angle_between, disp_text_header, TColor, )
+from cHelper import ( dig_for_Makefile, source_and_header_full_paths, find_README, find_executable, 
+                      make_clean, scrape_source, modify_makefile_to_disable_PIE, 
+                      make_in_dir_from_rule_with_output, count_SLOC_comments, )
 
 ##### Constants #####
 _CONFIG_PATH = 'HW_Config.json'
 
 
 
-########## STRING ANALYSIS #########################################################################
-
-def levenshtein_search_dist( s1 : str, s2 : str ) -> int:
-    """ Get the edit distance between two strings """
-    # Author: Salvador Dali, https://stackoverflow.com/a/32558749
-    # 1. Trivial cases: One string is empty
-    if not len(s1):
-        return len(s2)
-    if not len(s2):
-        return len(s1)
-    # 2. This algo assumes second string is at least as long as the first
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
-    if s1 in s2:
-        return 0
-    # 3. Compute distance and return
-    distances  = range(len(s1) + 1)
-    distances_ = None
-    for i2, c2 in enumerate(s2):
-        distances_ = deque()
-        distances_.append( i2+1 )
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-    return distances_.pop() + abs(len(s1)-len(s2)) # HACK
-
-
-
 ########## HELPER FUNCTIONS ########################################################################
-
-def dig_for_Makefile( drctry ):
-    """ WHERE IS THE ASSIGNMENT CODE? """
-    # Base Case: Assignment is here
-    mkPaths = [path for path in os.listdir( drctry ) if "akefile" in path]
-    if len( mkPaths ):
-        return drctry
-    # Recursive Case: Assignment is in a subfolder
-    subsubdirs = [p.path for p in os.scandir( drctry ) if p.is_dir()]
-    if not len( subsubdirs ):
-        return None
-    for ssd in subsubdirs:
-        # res = dig_for_Makefile( os.path.join( drctry, ssd ) )
-        res = dig_for_Makefile( ssd )
-        if res is not None:
-            return res
-    return None
-
-
-def modify_makefile_to_disable_PIE( hwDir ):
-    """ My lab workstation is STUPID """
-    mkPath = [path for path in os.listdir( hwDir ) if (("akefile" in path) and os.path.isfile( os.path.join( hwDir, path ) ))]
-    mkPath = os.path.join( hwDir, mkPath[0] )
-    modTxt = list()
-    with open( mkPath, 'r' ) as mkfl:
-        orgTxt = mkfl.readlines()
-    for line in orgTxt:
-        nuLine = line
-        if 'CFLG' in line[:5] and 'no-pie' not in line:
-            nuLine = f'{nuLine[:-1]} -no-pie\n'
-        if 'gcc' in line[:15] and 'no-pie' not in line:
-            nuLine = f'{nuLine[:-1]} -no-pie\n'
-        modTxt.append( nuLine )
-        
-    with open( mkPath, 'w' ) as mkfl:
-        mkfl.writelines( modTxt )
-
-
-def make_clean( hwDir ):
-    """ Get rid of Windows crap """
-    ruleStr = f"make clean -C '{hwDir}'"
-    process = subprocess.Popen( ruleStr, 
-                                shell  = True,
-                                stdout = subprocess.PIPE, 
-                                stderr = subprocess.PIPE )
-    # wait for the process to terminate
-    process.communicate()
-
-
-def scrape_source( hwDir, pattern ):
-    """ Attempt to find the `pattern` in the source at `hwDir` """
-    try:
-        out = subprocess.check_output( f"grep -n \"{pattern}\" {hwDir}/*.c*", 
-                                    stderr  = subprocess.STDOUT,
-                                    shell   = True,
-                                    timeout = 10 )
-        return out.decode()
-    except subprocess.CalledProcessError as err:
-        print( f"Scrape process crashed out:\n{err}\n" )
-        return ''
-    except subprocess.TimeoutExpired as err:
-        print( f"Scrape process timed out, Many files?:\n{err}\n" )
-        return ''
-
-
-def make_in_dir_from_rule_with_output( hwDir, rule, f ):
-    """ Make the rule in the child directory and report results """
-    runStudent = True
-
-    try:
-        ruleStr = f"make {rule} -C '{hwDir}'"
-
-        process = subprocess.Popen( ruleStr, 
-                                    shell  = True,
-                                    stdout = subprocess.PIPE, 
-                                    stderr = subprocess.PIPE )
-
-
-        # wait for the process to terminate
-        out, err = process.communicate()
-        out = out.decode()
-        err = err.decode()
-        errcode = process.returncode
-
-        out_line( f, f"COMPILE Command:  {ruleStr}" )
-
-        if (errcode == 0):
-            out_line( f, f"SUCCESS, Compilation Output:  {out}" )
-        else:
-            out_line( f, f"FAILURE, COMPILATION ERROR:  {err}\n{out}" )
-            runStudent = False
-
-    except Exception as e:
-        out_line( f, f"FAILURE, PROCESS ERROR:  {e}" )
-        runStudent = False
-
-    return runStudent
-
-
-def find_executable( drctry ):
-    """ Return the first executable encountered in `drctry` """
-    mkPaths = [path for path in os.listdir( drctry ) if "akefile" in path]
-    rtnPath = None
-    if len( mkPaths ):
-        mkPath = os.path.join( drctry, mkPaths[0] )
-        with open( mkPath, 'r' ) as f:
-            lines = f.readlines()
-            for line in lines:
-                elems = line.split(':')
-                if len( elems ) > 1:
-                    # print( elems )
-                    if (".c.o" not in elems[0]) and (".cpp.o" not in elems[0]) and ("all" not in elems[0]) and (elems[0].split('.')[-1] != 'o') and (elems[0].split('.')[-1] != 'a'):
-                        rtnPath = os.path.join( drctry, elems[0] )
-                        break
-    return rtnPath
-        
-
-def find_README( drctry ):
-    """ Return the first README encountered in `drctry` """
-    rdPaths = [path for path in os.listdir( drctry ) if ("readme" in str( path ).lower())]
-    if len( rdPaths ):
-        return os.path.join( drctry, rdPaths[0] )
-    else:
-        return None
-
-
-def source_and_header_full_paths( drctry ):
-    """ Get paths to all {*.c, *.cpp, *.h, *.hpp,} """
-    rtnPaths = list()
-    extMatch = ['c', 'cpp', 'h', 'hpp',]
-    srcPaths = [path for path in os.listdir( drctry )]
-    for sPath in srcPaths:
-        fPath = os.path.join( drctry, sPath )
-        if os.path.isfile( fPath ) and (str(fPath).split('.')[-1].lower() in extMatch):
-            rtnPaths.append( fPath )
-        elif os.path.isdir( fPath ):
-            rtnPaths.extend( source_and_header_full_paths( fPath ) )
-    return rtnPaths
-
-
-def tokenize( inStr ):
-    """ Split `inStr` into tokens we care about """
-    inStr    = str( inStr ) + ',' # Terminator hack
-    reserved = ['(',')',',',]
-    token    = ""
-    tokens   = []
-    for ch_i in inStr:
-        if (ch_i.isspace() or (ch_i in reserved)) and len( token ):
-            tokens.append( token )
-            token    = ""
-        else:
-            token += ch_i
-    return tokens
-
-
-def filter_float_tokens( tokens ):
-    """ Return a list of only the numeric tokens as floats """
-    rtnTkns = list()
-    for token in tokens:
-        try:
-            flt = float( token )
-            rtnTkns.append( flt )
-        except ValueError:
-            pass
-    return rtnTkns
-
-
-def vec_unit( v ):
-    """ Return the unit vector in the direction of `v` """
-    mag = np.linalg.norm( v )
-    if mag > 0.0:
-        return np.array( v ) / mag
-    else:
-        return v
-
-
-def angle_between( v1, v2 ):
-    """ Returns the angle in radians between vectors 'v1' and 'v2' """
-    # Adapted from code by: David Wolever, https://stackoverflow.com/a/13849249
-    v1_u  = vec_unit( v1 )
-    v2_u  = vec_unit( v2 )
-    vvDot = np.clip( np.dot( v1_u, v2_u ), -1.0, 1.0)
-    if abs( vvDot ) < 0.00001:
-        return np.pi / 2.0
-    else:
-        return np.arccos( vvDot )
-
-
-def count_SLOC_comments( fPath ):
-    """ Return code size metrics for this file """
-    Nsloc = 0
-    Ncmnt = 0
-    Nline = 0
-    lines = list()
-    multi = False
-    with open( fPath, 'r' ) as f:
-        try:
-            lines = f.readlines()
-            Nline = len( lines )
-        except Exception as e:
-            print( f"\nFAILED READ because {e}\n" )
-            return None, None, None
-        for line in lines:
-            if ("//" in line) or ("/*" in line) or ("*/" in line):
-                Ncmnt += 1
-                if ("/*" in line):
-                    multi = True
-                if ("*/" in line):
-                    multi = False
-            elif (not multi) and (len( line.strip() ) >= env_get("_MIN_LIN_CH")):
-                Nsloc += 1
-    return Nsloc, Ncmnt, Nline
-
 
 def attempt_normal_scan( fPath ):
     """ Attempt to determine if normals are correctly formed """
@@ -332,33 +93,6 @@ def attempt_normal_scan( fPath ):
     return feedback
 
 
-def concat_structs( op1, op2 ):
-    """ Combine two data structures, recursively """
-    if isinstance( op1, list ) and isinstance( op2, (list, deque,) ):
-        rtnLst = deque()
-        rtnLst.extend( op1 )
-        rtnLst.extend( op2 )
-        return list( rtnLst )
-    elif isinstance( op1, deque ) and isinstance( op2, (list, deque,) ):
-        rtnDqu = deque()
-        rtnDqu.extend( op1 )
-        rtnDqu.extend( op2 )
-        return rtnDqu
-    elif isinstance( op1, dict ) and isinstance( op2, dict ):
-        rtnDct = dict()
-        for k1, v1 in op1.items():
-            if k1 in op2:
-                rtnDct[ k1 ] = concat_structs( v1, op2[ k1 ] )
-            else:
-                rtnDct[ k1 ] = v1
-        for k2, v2 in op1.items():
-            if k2 not in rtnDct:
-                rtnDct[ k2 ] = v2
-        return rtnDct
-    else:
-        return None
-
-
 def find_processes( q : str ) -> list[str]:
     """ `grep` all running processes for `q` """
     ruleStr = f"ps ax | grep {q}"
@@ -410,30 +144,62 @@ def run_cmd_bg( cmd : str ) -> bool:
         print( f"Tried to run \"{cmd}\", BUT {e} happened!" )
         return False
 
-########## PRINT HELPERS ###########################################################################
 
-class TColor:
-    """ Terminal Colors """
-    # Source: https://stackoverflow.com/a/287944
-    HEADER    = '\033[95m'
-    OKBLUE    = '\033[94m'
-    OKCYAN    = '\033[96m'
-    OKGREEN   = '\033[92m'
-    WARNING   = '\033[93m'
-    FAIL      = '\033[91m'
-    ENDC      = '\033[0m'
-    BOLD      = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-def disp_text_header( titleStr : str, emphasis : int, preNL : int = 0, postNL : int = 0 ):
-    """ Make the headers that you like so much """
-    emphStr = '#'*int(emphasis)
-    newLine = '\n'
-    print( f"{newLine*int(preNL) if preNL else ''}{emphStr} {TColor.OKGREEN}{titleStr}{TColor.ENDC} {emphStr}{newLine*int(postNL) if postNL else ''}" )
+def file_EXT_full_paths( drctry, exts : str | list[str] ):
+    """ Get paths to all files that match the list of `exts` """
+    rtnPaths = deque()
+    extMatch = exts[:] if isinstance( exts, list ) else [f"{exts}",]
+    srcPaths = [path for path in os.listdir( drctry )]
+    for sPath in srcPaths:
+        fPath = os.path.join( drctry, sPath )
+        if os.path.isfile( fPath ) and (str(fPath).split('.')[-1].lower() in extMatch):
+            rtnPaths.append( fPath )
+        elif os.path.isdir( fPath ):
+            rtnPaths.extend( source_and_header_full_paths( fPath ) )
+    return list( rtnPaths )
 
 
 
+########## TEXTURES ################################################################################
+from PIL import Image
+
+def get_img_dims( imgPath : str ):
+    """ Get the image dimensions """
+    with Image.open( imgPath ) as im:
+        return im.width, im.height
+
+
+def p_image_square( imgPath : str ):
+    """ Return True if the the image dimensions are identical """
+    w, h = get_img_dims( imgPath )
+    return (w == h)
+
+
+def p_int_pow_2( n : int ):
+    """ Return True if `n` is an even power of 2 """
+    # Author: john-hen, https://stackoverflow.com/a/57027610
+    return (n != 0) and (n & (n-1) == 0)
+
+
+def p_img_dims_pow_2( imgPath : str ):
+    """ Return True if the the image dimensions are an even power of 2 """
+    w, h = get_img_dims( imgPath )
+    return (p_int_pow_2(w) and p_int_pow_2(h))
+
+
+def p_textures_OK( drctry : str, exts : list[str] = None ):
+    """ Inspect all textures in the directory, Return True if they are all okay, Print violations """
+    if exts is None:
+        exts = ["bmp",]
+    txtrPths = file_EXT_full_paths( drctry, exts )
+    rtnOK    = True
+    for tPath in txtrPths:
+        tOK   = p_img_dims_pow_2( tPath ) and p_image_square( tPath )
+        if not tOK:
+            print( f"WEIRD SIZE - File: {tPath}, Dims: {get_img_dims(tPath)}" )
+        rtnOK = rtnOK and tOK 
+    print()
+    return rtnOK
 
 
 
@@ -813,7 +579,7 @@ class GraphicsInspector:
             ##### Erase Previous Out Files #######
             make_clean( hwDir )
 
-            ##### Zip File Depth #################
+            ##### Lighting && Texturing ##########
             if "Normal Scan" in self.rubric:
                 out_line( f, f"##### Normal Scan for: {self.stdNam} #####\n" )
                 srcPaths = [path for path in source_and_header_full_paths( hwDir ) if ((".h" not in path) and (".hpp" not in path))] 
@@ -829,6 +595,11 @@ class GraphicsInspector:
                 if noNormal:
                     rubric["Feedback"]["GEOMETRY"].append( ">>>>>! NO NORMAL DATA: LIGHTING NOT ATTEMPTED !<<<<<" )
                 handle_rubric_item( "Normal Scan" )
+
+            if "Texture Scan" in self.rubric:
+                out_line( f, f"##### Texture Scan for: {self.stdNam} #####\n" )
+                print( f"Textures OK?: {p_textures_OK(hwDir,['bmp',])}" )
+                handle_rubric_item( "Texture Scan" )
 
             ##### Find and Display README ########
             fRead = find_README( hwDir )
@@ -870,12 +641,24 @@ class GraphicsInspector:
             handle_rubric_item( "Correct Executable Name", execName )
 
             if runStudent:
+                runDir = os.path.join( fExec.split('/')[:-1] )
                 print( f"Executable NAME: {execName}\n" )
                 print( f"Running ./{fExec} ..." )
-                os.system( f"./{fExec}" )
+                # 2025-10-17: Allow the program to access the local dir?
+                lastdir = os.getcwd() # Cache the current working directory
+                print( f"Switching from {lastdir} --to-> {runDir}" )
+                os.chdir( runDir ) # Change the current working directory
+                os.system( f"./{execName}" )
+                print( f"Switching from {runDir} --to-> {lastdir}" )
+                os.chdir( lastdir ) # Restore the current working directory
             else:
-                os.system( f"./{hwDir}/{env_get('_GOOD_NAME')}" )
-                out_line( f, f"ERROR: >>NO<< executable found at the expected location!" )
+                # 2025-10-17: Allow the program to access the local dir?
+                lastdir = os.getcwd() # Cache the current working directory
+                print( f"Switching from {lastdir} --to-> {hwDir}" )
+                os.chdir( hwDir ) # Change the current working directory
+                os.system( f"./{env_get('_GOOD_NAME')}" )
+                print( f"Switching from {hwDir} --to-> {lastdir}" )
+                os.chdir( lastdir ) # Restore the current working directory
             if not runStudent:
                 out_line( f, f"########## NOTIFY Student! ##########\n\n" )
 
@@ -1134,5 +917,5 @@ if __name__ == "__main__":
     grin = GraphicsInspector( _CONFIG_PATH )
     grin.startup()
     grin.run_grading_session()
-    sleep( 1.5 )
+    sleep( 0.5 )
     grin.shutdown()
--- Academic/GraphicsGrading/utils.py
+++ Academic/GraphicsGrading/utils.py
@@ -6,11 +6,13 @@ from __future__ import annotations
 import time, json, os
 now = time.time
 from os import environ
+from collections import deque
+
+import numpy as np
 
 
 
 ########## CONFIG HELPERS ##########################################################################
-
 
 def env_get( varName ):
     """ Get value from env """
@@ -87,3 +89,140 @@ def p_executable( path ):
     """ Return True if the file can be run """
     # Author: ssam, https://ubuntuforums.org/showthread.php?t=1457094&s=d79fc51840d2783bb1461f9a049b7e03&p=9138623#post9138623
     return bool( os.access(path, os.X_OK) )
+
+
+
+########## CONTAINERS ##############################################################################
+
+def concat_structs( op1, op2 ):
+    """ Combine two data structures, recursively """
+    if isinstance( op1, list ) and isinstance( op2, (list, deque,) ):
+        rtnLst = deque()
+        rtnLst.extend( op1 )
+        rtnLst.extend( op2 )
+        return list( rtnLst )
+    elif isinstance( op1, deque ) and isinstance( op2, (list, deque,) ):
+        rtnDqu = deque()
+        rtnDqu.extend( op1 )
+        rtnDqu.extend( op2 )
+        return rtnDqu
+    elif isinstance( op1, dict ) and isinstance( op2, dict ):
+        rtnDct = dict()
+        for k1, v1 in op1.items():
+            if k1 in op2:
+                rtnDct[ k1 ] = concat_structs( v1, op2[ k1 ] )
+            else:
+                rtnDct[ k1 ] = v1
+        for k2, v2 in op1.items():
+            if k2 not in rtnDct:
+                rtnDct[ k2 ] = v2
+        return rtnDct
+    else:
+        return None
+
+
+########## STRING ANALYSIS #########################################################################
+
+def levenshtein_search_dist( s1 : str, s2 : str ) -> int:
+    """ Get the edit distance between two strings """
+    # Author: Salvador Dali, https://stackoverflow.com/a/32558749
+    # 1. Trivial cases: One string is empty
+    if not len(s1):
+        return len(s2)
+    if not len(s2):
+        return len(s1)
+    # 2. This algo assumes second string is at least as long as the first
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+    if s1 in s2:
+        return 0
+    # 3. Compute distance and return
+    distances  = range(len(s1) + 1)
+    distances_ = None
+    for i2, c2 in enumerate(s2):
+        distances_ = deque()
+        distances_.append( i2+1 )
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+    return distances_.pop() + abs(len(s1)-len(s2)) # HACK
+
+
+def tokenize( inStr ):
+    """ Split `inStr` into tokens we care about """
+    inStr    = str( inStr ) + ',' # Terminator hack
+    reserved = ['(',')',',',]
+    token    = ""
+    tokens   = []
+    for ch_i in inStr:
+        if (ch_i.isspace() or (ch_i in reserved)) and len( token ):
+            tokens.append( token )
+            token    = ""
+        else:
+            token += ch_i
+    return tokens
+
+
+def filter_float_tokens( tokens ):
+    """ Return a list of only the numeric tokens as floats """
+    rtnTkns = list()
+    for token in tokens:
+        try:
+            flt = float( token )
+            rtnTkns.append( flt )
+        except ValueError:
+            pass
+    return rtnTkns
+
+
+
+########## GEOMETRY ################################################################################
+
+def vec_unit( v ):
+    """ Return the unit vector in the direction of `v` """
+    mag = np.linalg.norm( v )
+    if mag > 0.0:
+        return np.array( v ) / mag
+    else:
+        return v
+
+
+def angle_between( v1, v2 ):
+    """ Returns the angle in radians between vectors 'v1' and 'v2' """
+    # Adapted from code by: David Wolever, https://stackoverflow.com/a/13849249
+    v1_u  = vec_unit( v1 )
+    v2_u  = vec_unit( v2 )
+    vvDot = np.clip( np.dot( v1_u, v2_u ), -1.0, 1.0)
+    if abs( vvDot ) < 0.00001:
+        return np.pi / 2.0
+    else:
+        return np.arccos( vvDot )
+
+
+
+########## PRINT HELPERS ###########################################################################
+
+class TColor:
+    """ Terminal Colors """
+    # Source: https://stackoverflow.com/a/287944
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKCYAN    = '\033[96m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    ENDC      = '\033[0m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def disp_text_header( titleStr : str, emphasis : int, preNL : int = 0, postNL : int = 0 ):
+    """ Make the headers that you like so much """
+    emphStr = '#'*int(emphasis)
+    newLine = '\n'
+    print( f"{newLine*int(preNL) if preNL else ''}{emphStr} {TColor.OKGREEN}{titleStr}{TColor.ENDC} {emphStr}{newLine*int(postNL) if postNL else ''}" )
+
+
+
